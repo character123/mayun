4d35094b8dcf43c5158b69ec5fdb1d950a043b28
ldsheng <liudongsheng@sunlands.com>
2019-06-18 13:41:37 +0800

    init

---
+++ com/aliware/tianchi/UserLoadBalance.java
@@ -0,0 +1,93 @@
+package com.aliware.tianchi;
+
+/**
+ * Created by lds on 2019/6/18.
+ */
+import org.apache.dubbo.common.Constants;
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.cluster.LoadBalance;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author daofeng.xjf
+ *
+ * 负载均衡扩展接口
+ * 必选接口，核心接口
+ * 此类可以修改实现，不可以移动类或者修改包名
+ * 选手需要基于此类实现自己的负载均衡算法
+ */
+public class UserLoadBalance implements LoadBalance {
+
+    Random  random = new Random();
+
+    @Override
+    public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
+        if (invokers == null || invokers.isEmpty())
+            return null;
+        //如果只有一个提供者直接返回，预热失效
+        if (invokers.size() == 1)
+            return invokers.get(0);
+        return doSelect(invokers, url, invocation);
+    }
+
+
+    //让子类实现doSelect
+    private  <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation){
+        int length = invokers.size(); // Number of invokers
+        int totalWeight = 0; // The sum of weights
+        boolean sameWeight = true; // Every invoker has the same weight?
+        for (int i = 0; i < length; i++) {
+            int weight = getWeight(invokers.get(i), invocation);
+            totalWeight += weight; // Sum
+            if (sameWeight && i > 0
+                    && weight != getWeight(invokers.get(i - 1), invocation)) {
+                sameWeight = false;
+            }
+        }
+        //如果提供者权重不一样，加权随机
+        if (totalWeight > 0 && !sameWeight) {
+            // If (not every invoker has the same weight & at least one invoker's weight>0), select randomly based on totalWeight.
+            int offset = random.nextInt(totalWeight);
+            // Return a invoker based on the random value.
+            for (int i = 0; i < length; i++) {
+                offset -= getWeight(invokers.get(i), invocation);
+                if (offset < 0) {
+                    return invokers.get(i);
+                }
+            }
+        }
+        //如果提供者权重都一样，普通随机
+        // If all invokers have the same weight value or totalWeight=0, return evenly.
+        return invokers.get(random.nextInt(length));
+    }
+
+    //计算预热权重
+    protected int getWeight(Invoker<?> invoker, Invocation invocation) {
+        int weight = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
+        if (weight > 0) {
+            long timestamp = invoker.getUrl().getParameter(Constants.REMOTE_TIMESTAMP_KEY, 0L);
+            if (timestamp > 0L) {
+                //提供者在线时长
+                int uptime = (int) (System.currentTimeMillis() - timestamp);
+                //预热时间默认10分钟
+                int warmup = invoker.getUrl().getParameter(Constants.WARMUP_KEY, Constants.DEFAULT_WARMUP);
+                if (uptime > 0 && uptime < warmup) {
+                    weight = calculateWarmupWeight(uptime, warmup, weight);
+                }
+            }
+        }
+        return weight;
+    }
+
+    //用于计算预热权重
+    static int calculateWarmupWeight(int uptime, int warmup, int weight) {
+        int ww = (int) ((float) uptime / ((float) warmup / (float) weight));
+        return ww < 1 ? 1 : (ww > weight ? weight : ww);
+    }
+}
